22133c25eced1f5021cbe42d666c65a190257327
Aric <aessig32@hotmail.com>
2018-03-02 14:08:18 -0600

    works, just add timer

--- simon.js
+++ simon.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 
 	function simonTurn() {
 		simonArr.push(randomColor());
-		console.log(simonArr);
+		// console.log(simonArr);
 		for (var i = 0; i < simonArr.length; i++) {
 			var item = simonArr[i];
 			timeOut(item)
@@ -22,7 +22,6 @@ $(document).ready(function(){
 			setTimeout(function(){ 
 				displayColor(item)}, 1000*i);
 		}
-		
 	}
 	function randomColor() { //gets random color, pushes it to array.
 		var rando = Math.floor(Math.random()*4);
@@ -32,29 +31,35 @@ $(document).ready(function(){
 	function displayColor(item) {
 			$("." + item).addClass("light");
 			setTimeout(function(){
-				$("." + item).removeClass("light")}, 500);
+				$("." + item).removeClass("light")}, 300);
 	}
 	function userTurn(val) {
 		myArr.push(val);
+		console.log(myArr);
 		displayColor(val);
 		if (myArr.length === simonArr.length) {
-			if (areEqual) {
+			if (areEqual()) {
 				myArr = [];
 				setTimeout(function(){
 				simonTurn()}, 1000); 
 				//displayScore() //write this
 			} else {
-				alert("Sorry, you have failed");
+				setTimeout(function(){
+				alert("Sorry, you have failed")}, 1000);
+				reset();
 			}
 		}
 	}//end userTurn
-	//function to compare arrays
-	var areEqual = function() {
+	function areEqual() {
 		if (JSON.stringify(myArr) === JSON.stringify(simonArr)) {
 			return true;
 		} else {
 			return false;
 		}
+	}
+	function reset(){
+		simonArr = [];
+		myArr = [];
 	}
 
 
